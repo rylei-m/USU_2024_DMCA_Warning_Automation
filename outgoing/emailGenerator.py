8ddfe67b736897ca74ddb18798c9ad9eb3a45da4
Rylei Mindrum <a02352206@usu.edu>
2024-07-31 12:56:45 -0600

    Email Sending Automation Base

--- outgoing/emailGenerator.py
+++ outgoing/emailGenerator.py
@@ -1,3 +1,25 @@
 # File for automatically generating an outgoing email that should include all required information
-import XMLforDMCA.parsedData
+# import XMLforDMCA.parsedData
+import smtplib
 
+"""
+The smtplib library is a powerful tool that allows you to send emails using the Simple Mail Transfer Protocol (SMTP). 
+To use the smtplib library, you must first set up an email account that can be used to send emails programmatically.
+"""
+
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+smtp_username = 'youremail@gmail.com'
+smtp_password = 'your password'
+
+from_email = 'youremail@gmail.com'
+to_email = 'recipient@example.com'
+subject = 'Hello, world!'
+body = 'This is a test email.'
+
+message = f'Subject: {subject}\n\n{body}'
+
+with smtplib.SMTP(smtp_server, smtp_port) as smtp:
+    smtp.starttls()
+    smtp.login(smtp_username, smtp_password)
+    smtp.sendmail(from_email, to_email, message)
