4135eefb8d9b5d8d4d57a8aeb0bbbae33f8a1dcb
Rylei Mindrum <a02352206@usu.edu>
2024-07-31 13:21:30 -0600

    Converting XML into individual pieces of data

--- incoming/parser.py
+++ incoming/parser.py
@@ -1,7 +1,15 @@
 import xml.etree.ElementTree as ET
-import XMLforDMCA.testFiles
 
-tree = ET.parse('demo.xml')
+tree = ET.parse('/home/ryleim/PycharmProjects/XMLforDMCA/XMLforDMCA/testFiles/demo.xml')
 root = tree.getroot()
 
+data = {}
 
+for child in root:
+    tag = child.tag
+    text = child.text.strip()
+
+    data[tag] = text
+
+for key, value in data.items():
+    print(f"{key.replace('_',' ').title()} = \"{value}\"")
