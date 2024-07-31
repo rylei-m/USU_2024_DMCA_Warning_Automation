0944585be7676413e10a91d14f911d9329cbbc6f
Rylei Mindrum <a02352206@usu.edu>
2024-07-31 14:24:31 -0600

    removal of unnessecary files

--- lxmlTypes/ParseFromString.py
+++
@@ -1,6 +0,0 @@
-from lxml import etree
-
-xml_dir = '/home/ryleim/PycharmProjects/XMLforDMCA/XMLforDMCA/testFiles/demo.xml'
-
-xml_string = '''<root><child name="example">Text</child></root>'''
-root = etree.fromstring(xml_string)
