8391757a07fb551517d0c7de18be7c0ccc327a15
Rylei Mindrum <a02352206@usu.edu>
2024-07-31 13:42:51 -0600

    Parser Adjustment For Email XML Format

--- incoming/parser.py
+++ incoming/parser.py
@@ -2,14 +2,27 @@ import xml.etree.ElementTree as ET
 
 tree = ET.parse('/home/ryleim/PycharmProjects/XMLforDMCA/XMLforDMCA/testFiles/demo.xml')
 root = tree.getroot()
+namespace = {'ns': 'http://www.acns.net/ACNS'}
 
 data = {}
 
-for child in root:
-    tag = child.tag
-    text = child.text.strip()
 
-    data[tag] = text
+def strip_namespace(tag):
+    return tag.split("}")[1] if "}" in tag else tag
 
-for key, value in data.items():
-    print(f"{key.replace('_',' ').title()} = \"{value}\"")
+
+def extract_and_print(element, ns):
+    for child in element:
+        tag = strip_namespace(child.tag)
+        text = child.text.strip() if child.text is not None else ''
+        if len(child.attrib) > 0:
+            for attr, value in child.attrib.items():
+                print(f"{tag}.{attr} = \"{value}\"")
+        if text:
+            print(f"{tag} = \"{text}\"")
+        extract_and_print(child, ns)
+        # data[tag] = text
+
+
+print(f"Root element: {strip_namespace(root.tag)}\n")
+extract_and_print(root, namespace)
