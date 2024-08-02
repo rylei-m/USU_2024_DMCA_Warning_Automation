596e7e8353ebf5089f89ce96f9df761fee01896f
Rylei M <rymindrum@gmail.com>
2024-08-02 14:09:12 -0600

    Fixed Sender, Added MIME, deleted unnessessary file

--- email/outgoing/EmailGenerator.py
+++ email/outgoing/EmailGenerator.py
@@ -1,4 +1,6 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 from XMLforDMCA.email.creation.EmailTemplate import generated_email_subject, generated_email_body
 from XMLforDMCA.email.outgoing.privateUserInfo.JsonConverter import (UserEmail, Password, EmailTo)
@@ -18,12 +20,21 @@ to_email = f"{EmailTo}"
 
 message = f'Subject: {generated_email_subject()}\n\n{generated_email_body()}'
 
+subject = generated_email_subject()
+body = generated_email_body()
+
+msg = MIMEMultipart()
+msg['From'] = from_email
+msg['To'] = to_email
+msg['Subject'] = subject
+msg.attach(MIMEText(body, 'plain'))
+
 
 def sender():
     with smtplib.SMTP(smtp_server, smtp_port) as smtp:
         smtp.starttls()
         smtp.login(smtp_username, smtp_password)
-        smtp.sendmail(from_email, to_email, message)
+        smtp.send_message(msg)
 
 
 print("Email sent successfully!")
