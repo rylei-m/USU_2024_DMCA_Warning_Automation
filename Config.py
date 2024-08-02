0878d25a7fd72e39bc304728d9fa355518e28884
Rylei M <rymindrum@gmail.com>
2024-08-02 15:15:25 -0600

    Testing

--- Config.py
+++ Config.py
@@ -1,4 +1,4 @@
-XML_PATH = '/home/ryleim/PycharmProjects/XMLforDMCA/XMLforDMCA/testFiles/demo3.xml'
+XML_PATH = '/home/ryleim/PycharmProjects/XMLforDMCA/XMLforDMCA/testFiles/demo.xml'
 JSON_DIR = '/home/ryleim/PycharmProjects/XMLforDMCA/XMLforDMCA/parsedData/JSONS'
 JSON_BASE_NAME = "parsedData"
 JSON_EXTENSION = ".json"
